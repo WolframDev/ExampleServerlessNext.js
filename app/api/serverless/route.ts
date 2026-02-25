5015dc48d4f9f3cf7f22061422afe6d56622f586
WSoto <ing.wsoto@gmail.com>
2026-02-25 20:22:58 +0000

    upd:error

--- app/api/serverless/route.ts
+++ app/api/serverless/route.ts
@@ -18,7 +18,12 @@ export async function GET() {
     }));
 
     return NextResponse.json(resultado);
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+  } catch (error: unknown) {
+    let mensaje = "Error desconocido";
+
+    if (error instanceof Error) {
+      mensaje = error.message;
+    }
+    return NextResponse.json({ error: mensaje }, { status: 500 });
   }
 }
